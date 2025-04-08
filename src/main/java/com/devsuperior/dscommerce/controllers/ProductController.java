a509ddb73d1176b204e463ca7a32285f9cf21a5b
Giovanni <gi.canella85@outlook.com>
2025-04-08 04:59:09 -0300

    GET Products Paged

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -9,11 +9,14 @@ import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 //Essa anotation serve para quando minha aplicacao rodar o
@@ -36,7 +39,19 @@ public class ProductController {
     //O id da URL tem que casar com o parametro do metodo, por isso eu coloco a anotation @PathVariable. Pq ai o parametro da URL vai direto pro parametro desse metodo
     @GetMapping(value = "/{id}") //Resposta de um Get nessa rota products
     public ProductDTO findById(@PathVariable Long id) {
-        ProductDTO dto = service.findById(id);
-        return dto;
+        return service.findById(id);
+    };
+
+    //Pageable serve para fazer a paginacao dos dados, em vez de mostrar uma cacetada deles
+    //Por padrao o Pageable retorna 20 dados mas eu posso mexer nisso no frontend ou colocando na URL um queryparam tipo: http://localhost:8080/products?size=12 ai vai exibir 12 produtos e vai mudar o numero de pagina pois eu dividi em cada pagina 12 produtos.
+    //Posso colocar outro queryparam tipo: http://localhost:8080/products?size=12&page=1 e ai vai aparecer no caso do produto 13 ate o 24 (12 produtos so que na segunda pagina). Se fosse http://localhost:8080/products?size=12&page=1 vai aparecer so o produto 25 pois temos 25 produtos e na ultima pagina vai ficar o produto que restou pra aparecer
+    //Posso colocar outro queryparam tipo: http://localhost:8080/products?size=12&page=0&sort=name -> isso significa: a primeira pagina (page = 0) de tamanho 12 (com 12 produtos cada pagina) e organizar quando mostrar por nome (sort=name)
+    //Posso colocar outro queryparam tipo: http://localhost:8080/products?size=12&page=0&sort=name,desc -> isso significa: a primeira pagina (page = 0) de tamanho 12 (com 12 produtos cada pagina) e organizar quando mostrar por nome de maneira descrescente (sort=name, desc)
+    //Entao voce tem uma infinita gama de possibilidades de paginacao e configuracoes somente colocando esse pageable nos metodos
+    //Concluimos a primeira parte do caso de uso Consultar catalogo, que eh: O sistema informa uma listagem paginada de nome, imagem e preço dos
+    //produtos, ordenada por nome. Agora falta mais algumas funcionalidades desse mesmo caso de uso, como consulta por nome que iremos fazer mais pra frente
+    @GetMapping //Resposta de um Get nessa rota products
+    public Page<ProductDTO> findAll(Pageable pageable) { // Nosso metodo retorna um Page. Esse metodo recebe um pageable de argumento e eu vou passar esse argumento para a chamada de metodo abaixo
+        return service.findAll(pageable); //Retorna o retorno do metodo findAll do ProductService passando esse pageable como parametro para esse metodo
     };
 }
--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 //O Service Depende do Repository
@@ -28,5 +31,12 @@ public class ProductService {
         Product product = result.get(); // Pegando agora o produto que esta dentro do objeto option armazenado na variavel result
         ProductDTO dto = new ProductDTO(product); //Converto em dto
         return dto;
+    };
+
+    @Transactional(readOnly = true) // somente uma leitura no banco
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable); // o repository foi la no banco e buscou todos os produtos passando o pageable (para fazer a busca paginada) como argumento e retornou um Page de Product e armazenou na varaivel result
+        // Na linha de cima o repository.findAll(pageable) retorna um Page de Product e abaixo eu preciso converter esse Page (que ja eh uma Stream) de products em um Page de ProductsDTO para depois passar esse DTO para o controller la na classe ProductController
+        return result.map(x -> new ProductDTO(x)); //Converto cada registro da minha Page de product em dto usando o map assim filtrar os dados (converter todos eles em ProductDTO). Agora temos um page de ProductDTO
     };
 }
